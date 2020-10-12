c889c3cb9de085a99ba612044dbe3c379f02a76d
Walliski <walliski@gmail.com>
2020-10-13 01:14:06 +0300

    Add initial stored procedures
    They can fetch the total score that you have based on steamid, and for
    which mode you are running. The idea is that the plugin will use the
    servers default mode for calculating score.
    
    The procedures also only look at maps that you have added into the local
    mappool with the Local Ranks mappool file and update command. This
    solves the issue of having to somehow sync the mapcycle file into SQL.

---
+++ CalculateMapRank.sql
@@ -0,0 +1,109 @@
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE GetMapScore(
+    IN p_SteamID32 INT(10),
+    IN p_MapCourseID INT(10),
+    IN p_Mode TINYINT(3),
+    IN p_Teleports SMALLINT(5),
+    OUT p_Score SMALLINT(5)
+)
+
+BEGIN
+    DECLARE Rank INT;
+    DECLARE TotalCount INT;
+    DECLARE Percent DECIMAL(6,4);
+    DECLARE Score INT DEFAULT 0;
+
+    IF p_Teleports = 0 THEN
+        SELECT count(RunTime)
+        FROM Times
+        WHERE RunTime <= (SELECT RunTime FROM Times WHERE SteamID32 = p_SteamID32 AND MapCourseID = p_MapCourseID AND Mode = p_Mode AND Style = 0 AND Teleports = 0)
+        AND MapCourseID = p_MapCourseID AND Mode = p_Mode AND Style = 0 AND Teleports = 0
+        ORDER BY RunTime
+        INTO Rank;
+    ELSE
+        SELECT count(RunTime)
+        FROM Times
+        WHERE RunTime <= (SELECT RunTime FROM Times WHERE SteamID32 = p_SteamID32 AND MapCourseID = p_MapCourseID AND Mode = p_Mode AND Style = 0 AND Teleports != 0)
+        AND MapCourseID = p_MapCourseID AND Mode = p_Mode AND Style = 0 AND Teleports != 0
+        ORDER BY RunTime
+        INTO Rank;
+    END IF;
+
+    IF p_Teleports = 0 THEN
+        SELECT count(RunTime)
+        FROM Times
+        WHERE MapCourseID = p_MapCourseID AND Mode = p_Mode AND Style = 0 AND Teleports = 0
+        INTO TotalCount;
+    ELSE
+        SELECT count(RunTime)
+        FROM Times
+        WHERE MapCourseID = p_MapCourseID AND Mode = p_Mode AND Style = 0 AND Teleports != 0
+        INTO TotalCount;
+    END IF;
+
+    SET Percent = 1.0 + (1.0/CAST(TotalCount AS DECIMAL)) - (CAST(Rank AS DECIMAL) / CAST(TotalCount AS DECIMAL));
+
+    IF p_Teleports = 0 THEN
+        SET Score = CEILING(200.0 * Percent);
+
+        CASE Rank
+            WHEN 1 THEN SET Score = Score + 600;
+            WHEN 2 THEN SET Score = Score + 500;
+            WHEN 3 THEN SET Score = Score + 400;
+            WHEN 4 THEN SET Score = Score + 375;
+            WHEN 5 THEN SET Score = Score + 350;
+            WHEN 6 THEN SET Score = Score + 325;
+            WHEN 7 THEN SET Score = Score + 300;
+            WHEN 8 THEN SET Score = Score + 275;
+            WHEN 9 THEN SET Score = Score + 250;
+            WHEN 10 THEN SET Score = Score + 225;
+            WHEN 11 THEN SET Score = Score + 200;
+            WHEN 12 THEN SET Score = Score + 175;
+            WHEN 13 THEN SET Score = Score + 150;
+            WHEN 14 THEN SET Score = Score + 125;
+            WHEN 15 THEN SET Score = Score + 100;
+            WHEN 16 THEN SET Score = Score + 90;
+            WHEN 17 THEN SET Score = Score + 80;
+            WHEN 18 THEN SET Score = Score + 70;
+            WHEN 19 THEN SET Score = Score + 60;
+            WHEN 20 THEN SET Score = Score + 50;
+            ELSE SET Score = Score;
+        END CASE;
+    ELSE
+        SET Score = CEILING(100.0 * Percent);
+
+        CASE Rank
+            WHEN 1 THEN SET Score = Score + 400;
+            WHEN 2 THEN SET Score = Score + 300;
+            WHEN 3 THEN SET Score = Score + 200;
+            WHEN 4 THEN SET Score = Score + 190;
+            WHEN 5 THEN SET Score = Score + 180;
+            WHEN 6 THEN SET Score = Score + 170;
+            WHEN 7 THEN SET Score = Score + 160;
+            WHEN 8 THEN SET Score = Score + 150;
+            WHEN 9 THEN SET Score = Score + 140;
+            WHEN 10 THEN SET Score = Score + 130;
+            WHEN 11 THEN SET Score = Score + 120;
+            WHEN 12 THEN SET Score = Score + 110;
+            WHEN 13 THEN SET Score = Score + 100;
+            WHEN 14 THEN SET Score = Score + 90;
+            WHEN 15 THEN SET Score = Score + 80;
+            WHEN 16 THEN SET Score = Score + 60;
+            WHEN 17 THEN SET Score = Score + 40;
+            WHEN 18 THEN SET Score = Score + 20;
+            WHEN 19 THEN SET Score = Score + 10;
+            WHEN 20 THEN SET Score = Score + 5;
+            ELSE SET Score = Score;
+        END CASE;
+    END IF;
+
+    SET p_Score = Score;
+
+    -- SELECT Rank,TotalCount,Percent,Score;
+END;
+//
+
+DELIMITER ;
+
+-- CALL GetMapScore(20935526, 3, 2, 0, @score);
---
+++ GetTotalRank.sql
@@ -0,0 +1,55 @@
+-- Get all Times for the maps that a player has run
+
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE GetTotalRank(
+    IN p_SteamID32 INT(10),
+    IN p_Mode TINYINT(3)
+)
+
+BEGIN
+    DECLARE Done INT DEFAULT FALSE;
+    DECLARE Map INT(10);
+    DECLARE Teleports SMALLINT(5);
+    DECLARE Score INT DEFAULT 0;
+
+    DECLARE maplist CURSOR
+    FOR
+        SELECT t.MapCourseID, t.Teleports
+        FROM
+            (SELECT mc.MapCourseID
+            FROM Maps m
+            LEFT JOIN MapCourses mc
+            ON m.MapID = mc.MapID
+            WHERE mc.Course = 0
+            AND m.InRankedPool != 0) ids
+        LEFT JOIN Times t
+        ON ids.MapCourseID = t.MapCourseID
+        WHERE t.Mode = p_Mode
+        AND t.SteamID32 = p_SteamID32
+        AND t.Style = 0;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET Done = TRUE;
+
+    open maplist;
+    read_loop: LOOP
+        FETCH maplist INTO Map, Teleports;
+
+        IF Done THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL GetMapScore(p_SteamID32, Map, p_Mode, Teleports, @score);
+        SET Score = Score + @score;
+    END LOOP;
+
+    CLOSE maplist;
+
+    SELECT Score;
+END;
+//
+
+DELIMITER ;
+
+-- CALL GetTotalRank(20935526, 2);
+
+
